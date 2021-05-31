02eca3c2074ba4b89a1e9f85d498c3aec1b045d9
TakayukiKiyohara <TakayukiKiyohara@gmail.com>
2021-05-31 23:20:03 +0900

    main.cppとmain.after.cppのコードに食い違いがったので。

--- Sample_13_02/Sample_13_02/main.cpp
+++ Sample_13_02/Sample_13_02/main.cpp
@@ -10,11 +10,11 @@ void InitRootSignature(RootSignature& rs);
 struct DirectionalLight
 {
     Vector3  color;
-    float pad0;         // パディング
+    float pad0;     // パディング
     Vector3  direction;
-    float pad1;         // パディング
-    Vector3 eyePos;     // 視点
-    float specPow;      // スペキュラの絞り
+    float pad1;     // パディング
+    Vector3 eyePos; // 視点
+    float specPow;  // スペキュラの絞り
 };
 
 ///////////////////////////////////////////////////////////////////
@@ -28,11 +28,12 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLi
     //////////////////////////////////////
     // ここから初期化を行うコードを記述する
     //////////////////////////////////////
+
     // ルートシグネチャを作成
     RootSignature rootSignature;
     InitRootSignature(rootSignature);
 
-    //ディレクションライト
+    // ディレクションライト
     DirectionalLight light;
     light.direction.x = 1.0f;
     light.direction.y = -1.0f;
@@ -46,19 +47,12 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLi
 
     // モデルを初期化
     ModelInitData modelInitData;
-
     // 人型モデルを初期化
     modelInitData.m_tkmFilePath = "Assets/modelData/sample.tkm";
     modelInitData.m_fxFilePath = "Assets/shader/model.fx";
     Model humanModel;
     humanModel.Init(modelInitData);
-    humanModel.UpdateWorldMatrix({ -50.0f, 0.0f, 0.0f }, g_quatIdentity, g_vec3One);
-
-    // ロボットモデルを初期化
-    modelInitData.m_tkmFilePath = "Assets/modelData/robo/robo.tkm";
-    Model roboModel;
-    roboModel.Init(modelInitData);
-    roboModel.UpdateWorldMatrix({ 50.0f, 0.0f, 0.0f }, g_quatIdentity, { 10.0f, 10.0f, 10.0f });
+    humanModel.UpdateWorldMatrix({ 0.0f, 0.0f, 0.0f }, g_quatIdentity, g_vec3One);
 
     // 背景モデルを初期化
     modelInitData.m_tkmFilePath = "Assets/modelData/bg/bg.tkm";
@@ -67,12 +61,12 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLi
 
     // step-1 半透明の球体モデルを初期化
 
-    Vector3 planePos = { 0.0f, 0.0f, 20.0f };
+    Vector3 planePos = { 0.0f, 160.0f, 20.0f };
 
     // G-Bufferを作成
-    RenderTarget albedRT;   // アルベドカラー書き込み用のレンダリングターゲット
+    RenderTarget albedRT; // アルベドカラー書き込み用のレンダリングターゲット
     albedRT.Create(FRAME_BUFFER_W, FRAME_BUFFER_H, 1, 1, DXGI_FORMAT_R8G8B8A8_UNORM, DXGI_FORMAT_D32_FLOAT);
-    RenderTarget normalRT;  // 法線書き込み用のレンダリングターゲット
+    RenderTarget normalRT; // 法線書き込み用のレンダリングターゲット
     normalRT.Create(
         FRAME_BUFFER_W,
         FRAME_BUFFER_H,
@@ -87,17 +81,15 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLi
         FRAME_BUFFER_H,
         1,
         1,
-        DXGI_FORMAT_R32G32B32A32_FLOAT, // ワールド座標を記録するので、32ビット浮動小数点バッファを利用する
+        DXGI_FORMAT_R32G32B32A32_FLOAT, // ワールド座標を記録するので、32ビット浮動小数点バッファーを利用する
         DXGI_FORMAT_UNKNOWN
     );
 
     // ポストエフェクト的にディファードライティングを行うためのスプライトを初期化
     SpriteInitData spriteInitData;
-
-    // 画面全体にレンダリングするので幅と高さはフレームバッファの幅と高さと同じ
+    // 画面全体にレンダリングするので幅と高さはフレームバッファーの幅と高さと同じ
     spriteInitData.m_width = FRAME_BUFFER_W;
     spriteInitData.m_height = FRAME_BUFFER_H;
-
     // ディファードライティングで使用するテクスチャを設定
     spriteInitData.m_textures[0] = &albedRT.GetRenderTargetTexture();
     spriteInitData.m_textures[1] = &normalRT.GetRenderTargetTexture();
@@ -108,7 +100,6 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLi
     spriteInitData.m_fxFilePath = "Assets/shader/sprite.fx";
     spriteInitData.m_expandConstantBuffer = &light;
     spriteInitData.m_expandConstantBufferSize = sizeof(light);
-
     // 初期化データを使ってスプライトを作成
     Sprite defferdLightinSpr;
     defferdLightinSpr.Init(spriteInitData);
@@ -131,7 +122,7 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLi
         {
             planePos.x += 1.0f;
         }
-        if (g_pad[0]->IsPress(enButtonUp))
+        if (g_pad[0]->IsPress(enButtonUp) )
         {
             planePos.z -= 1.0f;
         }
@@ -154,26 +145,22 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLi
 
         // まず、レンダリングターゲットとして設定できるようになるまで待つ
         renderContext.WaitUntilToPossibleSetRenderTargets(ARRAYSIZE(rts), rts);
-
         // レンダリングターゲットを設定
         renderContext.SetRenderTargets(ARRAYSIZE(rts), rts);
-
         // レンダリングターゲットをクリア
         renderContext.ClearRenderTargetViews(ARRAYSIZE(rts), rts);
         humanModel.Draw(renderContext);
         bgModel.Draw(renderContext);
-        roboModel.Draw(renderContext);
 
         // レンダリングターゲットへの書き込み待ち
         renderContext.WaitUntilFinishDrawingToRenderTargets(ARRAYSIZE(rts), rts);
 
-        // レンダリング先をフレームバッファに戻してスプライトをレンダリングする
+        // レンダリング先をフレームバッファーに戻してスプライトをレンダリングする
         g_graphicsEngine->ChangeRenderTargetToFrameBuffer(renderContext);
-
         // G-Bufferの内容を元にしてディファードライティング
         defferdLightinSpr.Draw(renderContext);
 
-        // step-2 半透明オブジェクトを描画
+        // step-2 深度ステンシルビューをG-Bufferを作成したときのものに変更する
 
         /////////////////////////////////////////
         // 絵を描くコードを書くのはここまで！！！
@@ -181,6 +168,7 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLi
         // レンダリング終了
         g_engine->EndFrame();
     }
+
     return 0;
 }
 
@@ -188,7 +176,7 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLi
 void InitRootSignature(RootSignature& rs)
 {
     rs.Init(D3D12_FILTER_MIN_MAG_MIP_LINEAR,
-            D3D12_TEXTURE_ADDRESS_MODE_WRAP,
-            D3D12_TEXTURE_ADDRESS_MODE_WRAP,
-            D3D12_TEXTURE_ADDRESS_MODE_WRAP);
+        D3D12_TEXTURE_ADDRESS_MODE_WRAP,
+        D3D12_TEXTURE_ADDRESS_MODE_WRAP,
+        D3D12_TEXTURE_ADDRESS_MODE_WRAP);
 }
