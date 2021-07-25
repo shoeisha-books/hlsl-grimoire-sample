e69d3105518e8e18f5b47de8b7895c9a7c9ad754
TakayukiKiyohara <TakayukiKiyohara@gmail.com>
2021-07-25 23:58:57 +0900

    ディファードレンダリングのチャプターで不具合が起きていたので修正。

--- Appendix_04_03/Appendix_04_03/stdafx.h
+++ Appendix_04_03/Appendix_04_03/stdafx.h
@@ -1,7 +1,8 @@
 ﻿#pragma once
 #define _CRT_SECURE_NO_WARNINGS
 
-#define TK_ENABLE_HDR		//定義でHDRレンダリングを有効にする。
+#define TK_ENABLE_HDR		            //定義でHDRレンダリングを有効にする。
+#define TK_ENABLE_ALPHA_TO_COVERAGE     //定義でαトゥカバレッジを有効にする。
 #define APPEND_04
 
 #include "MiniEngine.h"
